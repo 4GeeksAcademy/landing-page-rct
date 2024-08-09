7598769062c48d065dbeef64dbd99e25b60c11f1
Dariet-SP <159501959+Dariet-SP@users.noreply.github.com>
2024-08-09 21:20:36 +0000

    last

---
+++ src/js/component/cards.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+export const cards = () => {
+    return (
+        <><div className="mt-2" />
+        <div className="card-group mx-auto" style={{ maxWidth: 1000 }}>
+            <div className="card">
+                <img src="https://via.placeholder.com/500x320" className="card-img-top" alt="..." />
+                <div className="card-body">
+                    <h5 className="card-title">Card title 1</h5>
+                    <p className="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                </div>
+                <div class="card-footer">
+                    <small class="text-body-secondary">Last updated 3 mins ago</small>
+                </div>
+            </div>
+            <div className="card">
+                <img src="https://via.placeholder.com/500x320" className="card-img-top" alt="..." />
+                <div className="card-body">
+                    <h5 className="card-title">Card title 2</h5>
+                    <p className="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                </div>
+                <div class="card-footer">
+                    <small class="text-body-secondary">Last updated 3 mins ago</small>
+                </div>
+            </div>
+            <div className="card">
+                <img src="https://via.placeholder.com/500x320" className="card-img-top" alt="..." />
+                <div className="card-body">
+                    <h5 className="card-title">Card title 3</h5>
+                    <p className="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                </div>
+                <div class="card-footer">
+                    <small class="text-body-secondary">Last updated 3 mins ago</small>
+                </div>
+            </div>
+            <div className="card">
+                <img src="https://via.placeholder.com/500x320" className="card-img-top" alt="..." />
+                <div className="card-body">
+                    <h5 className="card-title">Card title 4</h5>
+                    <p className="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                </div>
+                <div class="card-footer">
+                    <small class="text-body-secondary">Last updated 3 mins ago</small>
+                </div>
+            </div>
+        </div>
+        <div className="mt-2"/></>
+    );
+};
+
